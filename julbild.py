091ae708184a4e0c0602616bc78ffcec894f3a1b
viktorNTI <43564374+viktorNTI@users.noreply.github.com>
2018-12-18 11:01:24 +0100

    fourth commit

--- julbild.py
+++ julbild.py
@@ -13,11 +13,52 @@ for i in range(50):
     forward(50)
     right(144)
 
-left(100)
+left(90)
 forward(200)
 
-write("Finaste Julstjärnan i hela universum", font=("Georgia", 14, "normal"))
-time.sleep(5)
+write("Finaste Julstjärnan i hela universum", font=("Georgia", 16, "normal"))
+time.sleep(4)
+
+left(90)
+forward(270)
+
+pencolor(255,247,0)
+
+for i in range(20):
+    forward(i * 10)
+    right(144)
+
+pencolor(90,255,0)
+
+write("Nej, jag är bättre.", font=("Georgia", 16, "normal"))
+time.sleep(4)
+
+back(100)
+right(90)
+right(90)
+right(90)
+forward(300)
+
+pencolor(0,255,235) 
+
+forward(50)
+left(90)
+
+forward(50)
+left(90)
+
+forward(50)
+left(90)
+
+forward(50)
+left(90)
+
+pencolor(11,0,255)
+
+write("Fyrkant är bättre än stjärna", font=("Georgia", 16, "normal"))
+time.sleep(4)
+
+done()
 
 
 

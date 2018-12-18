b85469dddeca334b018514ed4814d8f7c662c1a9
viktorNTI <43564374+viktorNTI@users.noreply.github.com>
2018-12-18 10:41:09 +0100

    third commit

---
+++ julbild.py
@@ -0,0 +1,25 @@
+from turtle import *
+import time
+
+speed(0)
+
+colormode(255)
+bgcolor(255,0,0)
+pencolor(255,255,255)
+
+pensize(4)
+
+for i in range(50):
+    forward(50)
+    right(144)
+
+left(100)
+forward(200)
+
+write("Finaste Julstjärnan i hela universum", font=("Georgia", 14, "normal"))
+time.sleep(5)
+
+
+
+
+
